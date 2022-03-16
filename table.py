ec88b7a4519448ae04407905786582e6c32299d6
girisakar365 <70592584+girisakar365@users.noreply.github.com>
2022-03-16 08:18:05 +0545

    Updates!

--- table.py
+++ table.py
@@ -1,8 +1,9 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
-table.field_names = ['Bid', 'Total Point','Cards']
+table.field_names = ['Player','Bid', 'Total Point','Cards']
 
-def show_table(bid: float or int, tp: float or int, card: str):
-    table.add_row((str(bid), str(tp), card))
+def show_table(data:tuple):
+    table.add_rows(data)
     print(table)
+    table.clear_rows()
